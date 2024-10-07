e079dcd5073bc16661780e6e8b4f1828f42feb2f
AnupamaSaranu <78635073+Annupama@users.noreply.github.com>
2024-10-07 16:13:05 +0530

    Made changes in Config.py

--- Config/Config.py
+++ Config/Config.py
@@ -2,9 +2,9 @@ class Config:
     BASE_URL = "https://opensource-demo.orangehrmlive.com/"
     USERNAME = "Admin"
     PASSWORD = "admin123"
-    DB_HOST = "localhost"
-    DB_USER = "sa"
-    DB_PASSWORD = "password"
-    DB_NAME = "OrangeHRM"
+    DB_HOST = "192.168.0.30"
+    DB_USER = "User11"
+    DB_PASSWORD = "CDev011@gfh"
+    DB_NAME = "Anupama"
     API_ENDPOINT = "https://opensource-demo.orangehrmlive.com/auth/validate"
     EXCEL_PATH = "Test_Data/Test_Data.xlsx"  # Excel file path for data-driven testing
